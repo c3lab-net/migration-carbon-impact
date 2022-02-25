ca0b60ec3ea07847672599f07ec0f255507303ed
Yibo Guo <nil.yibo@gmail.com>
2022-02-25 11:57:25 -0800

    Added script to record CPU/memory usage.

---
+++ scripts/usage/record-cpu-mem-usage.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+LOGFILE=cpu-mem-usage.log
+SAMPLE_INTERVAL=1
+
+END_TIME=${1:-86400}
+
+echo >&2 "Recording CPU/memory usage for the next "$END_TIME"s ..."
+
+function write_cpu_mem_usage()
+{
+    timestamp="$(date +'%Y-%m-%dT%H:%M:%S.%3N%z')"
+    line=$(echo -n "$timestamp,")
+    cpu_and_mem_line=$(top -n 1 -b | head -n 4 | tail -n 2)
+    cpu_line=$(echo "$cpu_and_mem_line" | head -n 1)
+    mem_line=$(echo "$cpu_and_mem_line" | tail -n 1)
+    # 2: (un-niced) user, 4: system, 6: niced user, 8: idle
+    line+=$(echo $cpu_line | awk '{printf "%.1f,%.1f,%.1f,", $2+$6, $4, $8}')
+    # 8: used, 6: free
+    line+=$(echo $mem_line | awk '{printf "%.1f,%.1f\n", $8, $6}')
+    echo $line | tee -a $LOGFILE
+}
+
+echo "timestamp,cpu-user,cpu-kernel,cpu-idle,mem-used,mem-free" > $LOGFILE
+for ((i=0; i < $END_TIME; i++))
+do
+    write_cpu_mem_usage &
+    sleep $SAMPLE_INTERVAL
+    wait
+done
+# wait
