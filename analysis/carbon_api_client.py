67221d33ba02faf9abdbbf26612263ad41dcdd82
Yibo Guo <nil.yibo@gmail.com>
2022-11-24 15:25:14 -0800

    Cleaned up prints.

--- analysis/carbon_api_client.py
+++ analysis/carbon_api_client.py
@@ -10,12 +10,34 @@ class TimestampdValue:
     timestamp: datetime
     value: float
 
+    def __str__(self) -> str:
+        return '(%s, %.3f)' % (
+            self.timestamp.isoformat(),
+            self.value
+        )
+
+    def __lt__(self, other):
+        if self.timestamp != other.timestamp:
+            return self.timestamp < other.timestamp
+        else:
+            return self.value < other.value
+
 @dataclass
 class CarbonIntensityData:
     cloud_vendor: str
     region: str
     iso: str
     timeseries: list[TimestampdValue]
+
+    def __str__(self) -> str:
+        if len(self.timeseries) == 0:
+            timeseries_str = 'timeseries (len=0)'
+        else:
+            timeseries_str = f'timeseries ([%s, %s], len={len(self.timeseries)})' % (
+                self.timeseries[0].timestamp,
+                self.timeseries[-1].timestamp
+            )
+        return f'CarbonIntensityData {self.cloud_vendor}:{self.region} ({self.iso}), {timeseries_str}'
 
 
 def call_sysnet_carbon_intensity_api(latitude: float, longitude: float, start: arrow.Arrow, end: arrow.Arrow) -> CarbonIntensityData:
@@ -29,14 +51,13 @@ def call_sysnet_carbon_intensity_api(latitude: float, longitude: float, start: a
     assert response.ok, "Carbon intensity lookup failed (%d): %s" % (response.status_code, response.text)
     response_json = response.json()
     iso = response_json['region']
-    print('iso:', iso)
     carbon_intensities = response_json['carbon_intensities']
     timeseries = []
     for element in carbon_intensities:
         timestamp = arrow.get(element['timestamp']).datetime
         carbon_intensity = float(element['carbon_intensity'])
         timeseries.append(TimestampdValue(timestamp, carbon_intensity))
-    return CarbonIntensityData(None, None, iso, timeseries)
+    return CarbonIntensityData(None, None, iso, sorted(timeseries))
 
 def call_sysnet_energy_mixture_api(latitude: float, longitude: float, start: arrow.Arrow, end: arrow.Arrow):
     url_get_energy_mixture = 'http://yeti-09.sysnet.ucsd.edu/energy-mixture/'
@@ -49,7 +70,6 @@ def call_sysnet_energy_mixture_api(latitude: float, longitude: float, start: arr
     assert response.ok, "Energy mixture lookup failed (%d): %s" % (response.status_code, response.text)
     response_json = response.json()
     electricity_region = response_json['region']
-    print('region:', electricity_region)
     l_power_by_fuel_type = response_json['power_by_fuel_type']
     data_timeseries = []
     for entry in l_power_by_fuel_type:
