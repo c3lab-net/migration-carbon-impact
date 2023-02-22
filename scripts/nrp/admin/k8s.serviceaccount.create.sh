3f9624093c2304ad67f17960f47de3f9f57c8ce8
Yibo Guo <nil.yibo@gmail.com>
2023-02-22 11:29:20 -0800

    Updated create serviceaccount script.

--- scripts/nrp/admin/k8s.serviceaccount.create.sh
+++ scripts/nrp/admin/k8s.serviceaccount.create.sh
@@ -2,6 +2,10 @@
 
 NAMESPACE=c3lab
 SERVICE_ACCOUNT=sched.carbonaware
+CONFIG_ADMIN=.kube/config_admin
+
+set -e
+# set -x
 
 # Source: https://ucsd-prp.gitlab.io/userdocs/running/scripts/
 
@@ -14,6 +18,7 @@ echo $TOKENNAME
 TOKEN=`kubectl get secret $TOKENNAME -o jsonpath='{.data.token}'| base64 --decode`
 # echo $TOKEN
 
+cd $HOME
 cp .kube/config .kube/config_sa.$SERVICE_ACCOUNT
 kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT config set-credentials $SERVICE_ACCOUNT --token=$TOKEN
 kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT config set-context --current --user=$SERVICE_ACCOUNT
@@ -21,6 +26,7 @@ kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT config view
 # kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT config unset users.http://cilogon.org/server<your_cilogon_user_id>
 kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT config unset users.$cilogon_url
 
-kubectl create rolebinding ${SERVICE_ACCOUNT}_sa --clusterrole=edit --serviceaccount=$NAMESPACE:$SERVICE_ACCOUNT
+# Note: this is the only command needing admin priviledge
+(set -x; kubectl --kubeconfig=$CONFIG_ADMIN create rolebinding ${SERVICE_ACCOUNT}_sa --clusterrole=view --serviceaccount=$NAMESPACE:$SERVICE_ACCOUNT)
 
 kubectl --kubeconfig=.kube/config_sa.$SERVICE_ACCOUNT get pods
