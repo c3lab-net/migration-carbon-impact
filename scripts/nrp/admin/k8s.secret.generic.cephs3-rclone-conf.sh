0d4c9b2b34dd3ff8eaef28b2f837cfc9949a14bf
Yibo Guo <nil.yibo@gmail.com>
2023-03-08 13:59:14 -0800

    Remove NRP admin/setup scripts.

--- scripts/nrp/admin/k8s.secret.generic.cephs3-rclone-conf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-# s3 config: https://docs.pacificresearchplatform.org/userdocs/storage/ceph-s3/
-rclone_conf="$(rclone config file | grep rclone.conf$)"
-kubectl create secret generic cephs3-rclone-conf --from-file=rclone.conf="$rclone_conf"
