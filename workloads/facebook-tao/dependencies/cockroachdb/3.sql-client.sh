ea1fedf80708e675db0e27122a13f186f98f9c7c
Yibo Guo <nil.yibo@gmail.com>
2023-05-16 17:57:56 -0700

    Added cockroach DB secrets.

--- workloads/facebook-tao/dependencies/cockroachdb/3.sql-client.sh
+++ workloads/facebook-tao/dependencies/cockroachdb/3.sql-client.sh
@@ -2,7 +2,22 @@
 
 kubectl create -f cockroachdb-client.yaml
 
-kubectl exec -it taobench-cockroachdb-client-secure \
+CRDB_DB="taobench"
+# CRDB_USER="taouser"
+# CRDB_PASSWORD="insert-password-here"
+
+kubectl exec -it taobench-cockroachdb-client \
     -- ./cockroach sql \
     --certs-dir=/cockroach-certs \
-    --host=taobench-cockroachdb-public
+    --host=taobench-cockroachdb-public \
+    --execute="\l;"
+    # --execute="CREATE DATABASE \"$CRDB_DB\";" \
+    # --execute="CREATE USER \"$CRDB_USER\" WITH PASSWORD '$CRDB_PASSWORD';" \
+    # --execute="GRANT admin TO \"$CRDB_USER\";"
+
+kubectl create secret generic taobench-cockroachdb-client \
+    --from-literal=CRDB_DB="$CRDB_DB" #\
+    # --from-literal=CRDB_USER="$CRDB_USER" \
+    # --from-literal=CRDB_PASSWORD="$CRDB_PASSWORD"
+
+kubectl delete -f cockroachdb-client.yaml
