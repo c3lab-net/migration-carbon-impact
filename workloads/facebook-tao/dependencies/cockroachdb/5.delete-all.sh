609721c7f082835547bca3094e136b7da5e3ba91
Yibo Guo <nil.yibo@gmail.com>
2023-05-24 13:52:05 -0700

    Separate deletion of pods/jobs and others in CRDB.

--- workloads/facebook-tao/dependencies/cockroachdb/5.delete-all.sh
+++ workloads/facebook-tao/dependencies/cockroachdb/5.delete-all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-kubectl delete pods,statefulsets,services,poddisruptionbudget,jobs,rolebinding,clusterrolebinding,role,clusterrole,serviceaccount,alertmanager,prometheus,prometheusrule,serviceMonitor -l app=taobench-cockroachdb
+kubectl delete pods,statefulsets,jobs -l app=taobench-cockroachdb
+kubectl delete services,poddisruptionbudget,rolebinding,clusterrolebinding,role,clusterrole,serviceaccount,alertmanager,prometheus,prometheusrule,serviceMonitor -l app=taobench-cockroachdb
