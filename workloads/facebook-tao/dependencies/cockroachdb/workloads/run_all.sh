130ccbcba86371d9b2a455efcee273f3ff743e88
Yibo Guo <nil.yibo@gmail.com>
2023-06-16 22:45:07 -0700

    Added run all script to sweep CRDB workloads.

---
+++ workloads/facebook-tao/dependencies/cockroachdb/workloads/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+cd "$(dirname "$0")" || exit 1
+
+set -x
+
+n=1
+while [ $n -le 16 ]; do
+    for workload in $(tail +2 cockroach-workloads.csv | cut -f 1 -d','); do
+            ./run.sh create $workload $n
+            k.wait_for_job_finish.sh crdb-$workload-"$n"x
+            ./run.sh delete $workload $n
+    done
+    n=$((n * 2))
+done
