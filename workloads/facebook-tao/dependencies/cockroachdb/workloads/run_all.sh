9c1f98af2a35e7a815f902c7d54856b5df3e3545
Yibo Guo <nil.yibo@gmail.com>
2023-06-20 17:02:48 -0700

    Keep CRDB jobs after they complete.

--- workloads/facebook-tao/dependencies/cockroachdb/workloads/run_all.sh
+++ workloads/facebook-tao/dependencies/cockroachdb/workloads/run_all.sh
@@ -2,14 +2,15 @@
 
 cd "$(dirname "$0")" || exit 1
 
+set -e
 set -x
 
 n=1
 while [ $n -le 16 ]; do
     for workload in $(tail +2 cockroach-workloads.csv | cut -f 1 -d','); do
             ./run.sh create $workload $n
-            k.wait_for_job_finish.sh crdb-$workload-"$n"x
-            ./run.sh delete $workload $n
+            k.wait_for_job_finish.sh crdb-$workload-"$n"x && true
+            # ./run.sh delete $workload $n
     done
     n=$((n * 2))
 done
