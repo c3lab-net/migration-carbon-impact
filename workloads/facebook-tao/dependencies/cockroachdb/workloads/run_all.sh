1008ad54aca1dcd5ed52b69a85fe9de48f904f37
Yibo Guo <nil.yibo@gmail.com>
2023-06-20 22:05:32 -0700

    Added automatic retry on job failure.

--- workloads/facebook-tao/dependencies/cockroachdb/workloads/run_all.sh
+++ workloads/facebook-tao/dependencies/cockroachdb/workloads/run_all.sh
@@ -8,9 +8,11 @@ set -x
 n=1
 while [ $n -le 16 ]; do
     for workload in $(tail +2 cockroach-workloads.csv | cut -f 1 -d','); do
+        while :; do
             ./run.sh create $workload $n
-            k.wait_for_job_finish.sh crdb-$workload-"$n"x && true
-            # ./run.sh delete $workload $n
+            k.wait_for_job_finish.sh crdb-$workload-"$n"x && break
+            ./run.sh delete $workload $n
+        done
     done
     n=$((n * 2))
 done
